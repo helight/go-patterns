1e5d10dbfd7487a0c884e6820c8edbb11e64a61f
helight <helight@debian.helight>
2018-10-17 15:26:25 +0800

    add make file

---
+++ src/behavioral/16_interpreter/interpreter.go
@@ -0,0 +1,45 @@
+package interpreter
+
+import "fmt"
+
+type Expression interface {
+	Interpret(context string) bool
+}
+
+type Sell struct {
+}
+func (s *Sell) Do_command() {
+	fmt.Println("do Sell command")
+}
+
+type Buy struct {
+}
+func (b *Buy) Do_command() {
+	fmt.Println("do buy command")
+}
+
+type Broker struct {
+	Sell Command
+	Buy Command
+}
+
+func (b *Broker) AddCommand(ctype string, command Command) {
+	if "sell" == ctype {
+		b.Sell = command
+	} else if "buy" == ctype {
+		b.Buy = command
+	} else {
+		fmt.Println("add wrong command")
+	}
+}
+
+func (b *Broker) DoSell() {
+	b.Sell.Do_command()
+}
+
+func (b *Broker) DoBuy() {
+	b.Buy.Do_command()
+}
+
+func main() {
+}
---
+++ src/behavioral/16_interpreter/interpreter_test.go
@@ -0,0 +1,19 @@
+package interpreter
+
+import (
+	"testing"
+)
+
+func TestChain(t *testing.T) {
+	sell := new(Sell)
+	buy := new(Buy)
+	broker := new(Broker)
+	broker.AddCommand("sell", sell)
+	broker.AddCommand("buy", buy)
+
+	broker.DoSell()
+	broker.DoBuy()
+}
+
+func init() {
+}
