6d63c20ab49756ef491c55f6f22263d13448307f
helight <helightxu@tencent.com>
2020-05-08 09:31:05 +0800

    add once singletion demo

--- src/creational/03_singleton/singleton.go
+++ src/creational/03_singleton/singleton.go
@@ -8,14 +8,17 @@ import (
 type Singleton struct {
 }
 
+var (
+	singleton *Singleton
+	singlock  sync.Mutex
+)
+
+// DoSomething just for test
 func (s *Singleton) DoSomething() {
-	fmt.Println("Tish is a Singleton")
+	fmt.Println("this is a Singleton")
 }
 
-var singleton *Singleton
-var singlock sync.Mutex
-
-//
+// Singleton return Singleton
 func GetSingleton() *Singleton {
 	singlock.Lock()
 	if singleton == nil {
@@ -23,10 +26,25 @@ func GetSingleton() *Singleton {
 	}
 	singlock.Unlock()
 	return singleton
+}
 
+var (
+	singleton2 *Singleton
+	done       sync.Once
+)
+
+// Singleton return Singleton
+func GetSingleton2() *Singleton {
+	done.Do(func() {
+		singleton2 = &Singleton{}
+	})
+	return singleton2
 }
 
 func main() {
 	singlet := GetSingleton()
 	singlet.DoSomething()
+
+	singlet2 := GetSingleton2()
+	singlet2.DoSomething()
 }
